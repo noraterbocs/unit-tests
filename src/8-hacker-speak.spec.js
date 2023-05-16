dfce0cbb6dc6572b94c76eb197484b187c24df18
Nora Terbocs <nora.terbocs@gmail.com>
2023-05-16 11:40:53 +0200

    Changed back test string

--- src/8-hacker-speak.spec.js
+++ src/8-hacker-speak.spec.js
@@ -2,7 +2,7 @@ import { hackerSpeak } from './8-hacker-speak'
 
 describe('8-hacker-speak', () => {
   it("should replace all 'a's with 4, 'e's with 3, 'i's with 1, 'o's with 0, and 's's with 5", () => {
-    expect(hackerSpeak("javascript is cooloo")).toEqual('j4v45cr1pt 15 c00l00')
+    expect(hackerSpeak("javascript is cool")).toEqual('j4v45cr1pt 15 c00l')
     expect(hackerSpeak("programming is fun")).toEqual('pr0gr4mm1ng 15 fun')
     expect(hackerSpeak("become a coder")).toEqual('b3c0m3 4 c0d3r')
   })
